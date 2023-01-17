ced88e9ed5d1e79cc8ec9164497df723e14f2efe
aanfadillah <115763475+aanfadillah@users.noreply.github.com>
2023-01-17 10:13:07 +0700

    Create Latihan1.py

---
+++ Latihan1.py
@@ -0,0 +1,37 @@
+print("Nama :Aan Fadillah Putra")
+
+print("NIM : 312210327")
+
+txt = 'Halo Dunia'
+
+mencetak()
+
+# Hitung jumlah karakternya
+
+cetak(len(txt))
+
+# Ambil karakter terakhir
+
+cetak(txt[-1])
+
+# Ambil indeks karakter ke-2 sampai indeks ke-4 (llo)
+
+cetak(txt[2:5])
+
+# Hilangkan spasi pada text tersebut (HelloWorld)
+
+cetak(txt.ganti(" ", ""))
+
+# Ubah teks menjadi huruf besar
+
+cetak(txt.atas())
+
+# Ubah teks menjadi huruf kecil
+
+cetak(txt.lower())
+
+# Ganti karakter H dengan karakter J
+
+cetak(txt.replace("H", "J"))
+
+mencetak()
