b96072ff340fbca3bd05a5b23c9fb8129df3ebbc
aanfadillah <115763475+aanfadillah@users.noreply.github.com>
2023-01-17 10:17:49 +0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+# Ini adalah contoh skrip Python.
+
+# Tekan Shift+F10 untuk menjalankannya atau menggantinya dengan kode Anda.
+# Tekan Double Shift untuk mencari kelas, file, jendela alat, tindakan, dan pengaturan di mana saja.
+
+
+def  print_hi ( nama ):
+    # Gunakan breakpoint pada baris kode di bawah untuk men-debug skrip Anda.
+    print ( f'Hai, { name } ' )   # Tekan Ctrl+F8 untuk mengaktifkan breakpoint.
+
+
+# Tekan tombol hijau di selokan untuk menjalankan skrip.
+jika  __nama__  ==  '__main__' :
+    print_hi ( 'PyCharm' )
+
+# Lihat bantuan PyCharm di https://www.jetbrains.com/help/pycharm/
